1582e066d46c3032456e1b948f6ef17efae40cae
kilroyjones <tsbach@gmail.com>
2020-12-25 23:59:24 +0800

    update

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Holding from "./Holding.svelte";
+	let holdings = "";
+	async function getHoldings() {
+		await fetch(`http://localhost:5000/holdings`)
+			.then((r) => r.json())
+			.then((data) => {
+				holdings = data;
+			});
+	}
+	getHoldings();
+</script>
+
+<div class="container">
+	<div class="columns">
+		<div class="column col-md-4" />
+		<div class="column col-md-4">Heading goes here</div>
+		<div class="column col-md-4" />
+	</div>
+</div>
+
+<div class="container">
+	<div class="columns">
+		<div class="column col-xs-4" />
+		<div class="column col-xs-4">
+			{#each holdings as holding}
+				<Holding {holding} />
+			{/each}
+		</div>
+		<div class="column col-xs-4" />
+	</div>
+</div>
---
+++ src/Holding.svelte
@@ -0,0 +1,40 @@
+<script>
+  export let holding;
+  let gain = holding.current_price - holding.purchased_price;
+</script>
+
+<style>
+  .gain {
+    font-size: 26px;
+    font-weight: 700;
+    color: green;
+  }
+  .loss {
+    font-size: 12px;
+    font-weight: 700;
+    color: red;
+  }
+  .current-price {
+    font-size: 14px;
+  }
+  .purchase-price {
+    font-size: 12px;
+  }
+</style>
+
+<div class="tile tile-centered">
+  <div class="tile-content">
+    <div class="tile-title">{holding.ticker}</div>
+    <small class="tile-subtitle text-gray">Purchased:
+      {holding.date_active}</small>
+  </div>
+  <div class="tile-action">
+    <div class="current-price text-right">{holding.current_price}</div>
+    <div class="purchase-price text-right">{holding.purchased_price}</div>
+  </div>
+  {#if { gain } < 0.0}
+    <div class="pl-2 loss tile-action">{gain}</div>
+  {:else}
+    <div class="pl-2 gain tile-action">{gain}</div>
+  {/if}
+</div>
